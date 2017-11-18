3a2fc4d1a93e6784f5f805dc6f2c6d519e2f3547
Kaus <kausr2595@gmail.com>
2017-11-18 10:50:14 +0530

    OOPS Concept

---
+++ OOP/class.py
@@ -0,0 +1,9 @@
+class first : #first is the class name
+    name='abcde'
+    hobby='code'
+    def display(self):
+        return 'I am a method'
+obj = first() #object of class first is created
+obj.name      #display the input in the variable name
+obj.hobby     #display the input in the variable code
+obj.display()  #calling of function display
